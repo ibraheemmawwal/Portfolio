e027f0d3a327739df4b2ff1eb8e2deced2edc6d7
ibraheemmawwal <64714175+ibraheemmawwal@users.noreply.github.com>
2022-04-18 00:14:39 +0100

    commit-2

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -35,6 +35,10 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import bitmoji from '../assets/bitmoji.png';
+import deploySvgIcon from '../assets/services-deploy.svg';
+import debugSvgIcon from '../assets/services-debug.svg';
+import apiSvgIcon from '../assets/services-api.svg';
 
 export default {
   email,
@@ -71,4 +75,8 @@ export default {
   nb,
   skype,
   spotify,
+  deploySvgIcon,
+  debugSvgIcon,
+  apiSvgIcon,
+  bitmoji,
 };
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,46 +1,240 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React from 'react';
+// import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
+import { images } from '../../constants';
 import './About.scss';
-import { urlFor, client } from '../../client';
-
+// import { urlFor, client } from '../../client';
 const About = () => {
-  const [abouts, setAbouts] = useState([]);
-
-  useEffect(() => {
-    const query = '*[_type == "abouts"]';
-
-    client.fetch(query).then((data) => {
-      setAbouts(data);
-    });
-  }, []);
-
   return (
     <>
-      <h2 className="head-text">
-        I Know that <span>Good Development</span> <br />
-        means <span>Good Business</span>
-      </h2>
-
-      <div className="app__profiles">
-        {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>
-              {about.title}
-            </h2>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
-        ))}
+      <div className="about-section-wrapper">
+        <div className="about-content">
+          <div className="about-header">
+            About <span className="color">Me</span>
+            <span className="header-caption">
+              <span className="color"></span>
+            </span>
+          </div>
+          <div className="about-main">
+            <div className="first-paragraph">
+              <span className="first-line">
+                I&#39;m a FullStack Engineer with a passion for building
+                <span className="color"> (JS-mainly)</span> based in Nigeria.{' '}
+              </span>
+              <br />
+              <span className="second-line">
+                I started my coding journey in 2018 after I left sec. school.
+                I&#39;m currently a student at University of Lagos studying
+                Electrical and Electronics Engineering.
+                <br />
+                Over the years I&#39;ve successfully developed custom solutions
+                to solve challenges and engineered designs to meet predetermined
+                functionality goals.
+                <br />
+                &mdash; Strengths: Debugging, learning new languages, testing.
+                <br />
+                &mdash; Weakness: writing plain CSS(pretty good using frameworks
+                though).
+                <br />
+                Coding and Tech has been so fun as no day passes without
+                learning new things or encountering a problem, well thanks to
+                community as been so great in helping find solutions .
+              </span>
+              <div className="cv">
+                <a
+                  href="https://drive.google.com/file/d/1nOV7ZzXMiOgVEHNJKebfvAkdlNk1YAmx/view?usp=sharing"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <button>Download CV</button>
+                </a>
+              </div>
+            </div>
+            <div className="about-img">
+              <div className="img-container">
+                <img
+                  src={images.bitmoji}
+                  placeholder="blur"
+                  alt="eniola-osabiya"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="services">
+          <div className="services-heading">
+            <span className="color">My</span> Services
+          </div>
+          <div className="services-content-wrapper">
+            <div className="service">
+              <div className="service-img">
+                <div className="img-container">
+                  <img
+                    src={images.debugSvgIcon}
+                    width={80}
+                    height={80}
+                    alt="debugging-service"
+                  />
+                </div>
+              </div>
+              <div className="service-description">
+                <h2>Debugging</h2>
+                <p>
+                  Debugging is the process of detecting and removing of existing
+                  and potential errors (bugs) in a software code that can cause
+                  it to behave unexpectedly or crash.
+                </p>
+              </div>
+            </div>
+            <div className="service">
+              <div className="service-img">
+                <div className="img-container">
+                  <img
+                    src={images.apiSvgIcon}
+                    width={80}
+                    height={80}
+                    alt="api-development-service"
+                  />
+                </div>
+              </div>
+              <div className="service-description">
+                <h2>API Development</h2>
+                <p>
+                  API (Application Programming Interface software) is a set of
+                  instructions, standards or requirements that enables a
+                  software or app to employ features/services of another app,
+                </p>
+              </div>
+            </div>
+            <div className="service">
+              <div className="service-img">
+                <div className="img-container">
+                  <img
+                    src={images.deploySvgIcon}
+                    width={80}
+                    height={80}
+                    alt="deployment-service"
+                  />
+                </div>
+              </div>
+              <div className="service-description">
+                <h2>Deployment</h2>
+                <p>
+                  Deploying projects/sites and apps to live servers using tools
+                  like Docker, AWS, Heroku, Netlify, Vercel, Google Play Console
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="skills">
+          <div className="skills-header">
+            My <span className="color"> Skills</span>
+          </div>
+          <div className="skills-content">
+            {/* <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">HTML/CSS</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '85%' }}>
+                  85%
+                </div>
+              </div>
+            </div> */}
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Working remotely</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '90%' }}>
+                  90%
+                </div>
+              </div>
+            </div>
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Time management</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '92%' }}>
+                  92%
+                </div>
+              </div>
+            </div>
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Working with team</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '88%' }}>
+                  88%
+                </div>
+              </div>
+            </div>
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Availaility</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '86%' }}>
+                  86%
+                </div>
+              </div>
+            </div>
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Learning Pace</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '80%' }}>
+                  80%
+                </div>
+              </div>
+            </div>
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Leardership</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '60%' }}>
+                  91%
+                </div>
+              </div>
+            </div>
+            {/* <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner"></div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '90%' }}>
+                  90%
+                </div>
+              </div>
+            </div>
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Documentation</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '87%' }}>
+                  87%
+                </div>
+              </div>
+            </div>
+            <div className="skill">
+              <div className="skill-text">
+                <div className="skill-text-inner">Git / GitHub</div>
+              </div>
+              <div className="progress-wrapper">
+                <div className="progress-inner" style={{ width: '97%' }}>
+                  97%
+                </div>
+              </div>
+            </div> */}
+          </div>
+        </div>
       </div>
     </>
   );
